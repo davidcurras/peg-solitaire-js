6c8c7e14c462d0eba36ed19def3e56827cea62c6
Esteban Frare <esteban.frare@radiumrocket.com>
2019-07-11 00:34:55 -0300

    PS2019: show suggestions

--- js/app.js
+++ js/app.js
@@ -10,9 +10,26 @@ var board = [
 
 var selectedPeg = { x: undefined, y: undefined }
 
+var createId = function(rowN, colN) {
+  // create id with the row and the col number
+  return 'peg-' + rowN + '-' + colN
+};
+
+var getPositionFromId = function(peg) {
+  var idParts = peg.id && peg.id.length ? peg.id.split('-') : []
+  if (idParts.length === 3) {
+    return {
+      x: parseInt(idParts[1]),
+      y: parseInt(idParts[2])
+    }
+  }
+  return {}
+}
+
+
 var generateCell = function(cell, rowN, colN) {
   // initial html for button with row and column position for id
-  var html = '<button id="peg-'+rowN+'-'+colN+'" class="'
+  var html = '<button id=' + createId(rowN, colN) + ' class="'
   // if cell has value=1 add class "peg"
   if(cell && cell.value) html += 'peg'
   // if cell has value=0 add class "hole"
@@ -42,16 +59,67 @@ var generateBoard = function() {
   return html
 }
 
+var unselectPeg = function(id) {
+  if (selectedPeg.x !== undefined && selectedPeg.y !== undefined) {
+    // Generate the id of the previous selected
+    var prevSelectedId = createId(selectedPeg.x, selectedPeg.y)
+    document.getElementById(prevSelectedId).className = 'peg'
+    // Remove the previous suggestions
+    var suggestion = document.getElementsByClassName('suggestion')
+    for (var i = 0; i < suggestion.length; i++) {
+      suggestion[i].className = 'hole'
+    }
+  }
+}
+
+var getElement = function(id) {
+  // get element if it exists
+  var element = document.getElementById(id)
+  return element || {}
+}
+
+var showSuggestions = function() {
+  var near = {
+    above: getElement(createId(selectedPeg.x - 1, selectedPeg.y)),
+    left: getElement(createId(selectedPeg.x, selectedPeg.y - 1)),
+    right: getElement(createId(selectedPeg.x, selectedPeg.y + 1)),
+    below: getElement(createId(selectedPeg.x + 1, selectedPeg.y))
+  }
+
+  var possible = {
+    above: getElement(createId(selectedPeg.x - 2, selectedPeg.y)),
+    left: getElement(createId(selectedPeg.x, selectedPeg.y - 2)),
+    right: getElement(createId(selectedPeg.x, selectedPeg.y + 2)),
+    below: getElement(createId(selectedPeg.x + 2, selectedPeg.y))
+  }
+
+
+  Object.keys(near).forEach(function(side){ 
+    // check if the positions next to it are selected
+    // and check if possible positions are not selected
+    if (near[side].className === 'peg' && possible[side].className === 'hole') {
+      // show the possible position
+      possible[side].className = 'suggestion'
+    }
+  })
+}
+
 var selectPeg = function(evt) {
   // Get selected html element from event
   var peg = evt.target
   // Parse row and column from element id
-  var idParts = peg.id && peg.id.length
-    ? peg.id.split('-') : []
-  if(idParts.length === 3) {
-    selectedPeg.x = idParts[1]
-    selectedPeg.y = idParts[2]
-    peg.className = 'selected'
+  var position = getPositionFromId(peg)
+  if (position.x !== undefined && position.y !== undefined) {
+    unselectPeg();
+    if (selectedPeg.x === position.x && selectedPeg.y === position.y) {
+      selectedPeg.x = undefined
+      selectedPeg.y = undefined
+    } else {
+      selectedPeg.x = position.x
+      selectedPeg.y = position.y
+      peg.className = 'selected'
+      showSuggestions()
+    }
   }
 }
 
